5fe07b82f08f4c1ae68bb5e98d6c1f350e461108
Beyza Armağan <70449529+beyza-armagan@users.noreply.github.com>
2023-12-22 10:45:18 +0300

    Update Header.test.js

--- src/test/Header.test.js
+++ src/test/Header.test.js
@@ -1,3 +1,8 @@
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import App from "../App";
+
 describe("render the header", () => {
 
   beforeEach(() => {
